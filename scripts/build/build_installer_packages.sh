ac4f0a3d42c5cccc10796aff9efdc9f2b244aed6
Ermal Luçi <eri@pfsense.org>
2013-12-05 15:08:05 +0100

    Use proper pkg tools depending on FreeBSD version

--- scripts/build/build_installer_packages.sh
+++ scripts/build/build_installer_packages.sh
@@ -67,17 +67,26 @@ rebuild_port()
 	chmod -R 777 work			&& \
 	make options_UNSET="${options_UNSET}" options_SET="${options_SET}" $WITH_NLS_DEF $WITH_CURSES_DEF $WITH_CGI_DEF $WITH_QT_DEF \
 	     $WITH_LUA_BACKEND_DEF $WITH_C_BACKEND_DEF $WITH_DEBUG_DEF \
-	     $PORTS_FLAGS package FORCE_PKG_REGISTER=yes	&& \
-	rm -rf work
+	     $PORTS_FLAGS package FORCE_PKG_REGISTER=yes && \
+	make clean && rm -rf work
 }
 
-su root -c \
-   "pkg_delete -f 'libaura-*'
-    pkg_delete -f 'libinstaller-*'
-    pkg_delete -f '*dfui*'
-    pkg_delete -f 'thttpd-notimeout-*'
-    pkg_delete -f 'lua50-*'
-    pkg_delete -f 'bsdinstaller-*'"
+if [ $FREEBSD_VERSION < 9 ]; then
+	su root -c \
+   	"pkg_delete -f 'libaura-*'
+    	pkg_delete -f 'libinstaller-*'
+    	pkg_delete -f '*dfui*'
+    	pkg_delete -f 'thttpd-notimeout-*'
+    	pkg_delete -f 'lua50-*'
+    	pkg_delete -f 'bsdinstaller-*'"
+else
+	su root -c \
+	"pkg delete -yf 'libaura-*'
+    	pkg delete -yf 'libinstaller-*'
+    	pkg delete -yf '*dfui*'
+    	pkg delete -yf 'thttpd-notimeout-*'
+    	pkg delete -yf 'lua50-*'
+    	pkg delete -yf 'bsdinstaller-*'" 
 
 if [ "X$REMOVEOLDPKGS" = "XYES" ]; then
 	rm -rf $PACKAGESDIR/libaura-*.????.????.t?z
